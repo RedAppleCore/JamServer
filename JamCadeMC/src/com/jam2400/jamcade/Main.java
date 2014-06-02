c591d2b474ae4f6aca5cdd8d61c80ebde84787df
JamDev <jamesc2400@gmail.com>
2014-06-02 20:16:12 +0100

    Added first command!

--- JamCadeMC/src/com/jam2400/jamcade/Main.java
+++ JamCadeMC/src/com/jam2400/jamcade/Main.java
@@ -1,5 +1,9 @@
 package com.jam2400.jamcade;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -13,5 +17,18 @@ public class Main extends JavaPlugin {
 	public void onDisable(){
 		getLogger().info("JamCade disabled.");
 	}
+	
+	public boolean onCommmand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(cmd.equals("hi")){
+			Player player = (Player) sender;
+			player.sendMessage(ChatColor.GOLD + "Hi");
+			
+			
+		}
+		
+		return false;
+	}
 
 }
+
+
