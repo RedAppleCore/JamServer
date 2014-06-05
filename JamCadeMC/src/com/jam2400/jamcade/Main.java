4017a23e5ff7c19c11684cfaba642b909d080207
JamDev <jamesc2400@gmail.com>
2014-06-05 20:24:57 +0100

    Added a fun block trail, unstable!

--- JamCadeMC/src/com/jam2400/jamcade/Main.java
+++ JamCadeMC/src/com/jam2400/jamcade/Main.java
@@ -1,16 +1,24 @@
 package com.jam2400.jamcade;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
 	
 	@Override
 	public void onEnable(){
 		getLogger().info("JamCade is intiated!");
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
 	}
 	
 	@Override
@@ -19,14 +27,27 @@ public class Main extends JavaPlugin {
 	}
 	
 	public boolean onCommmand(CommandSender sender, Command cmd, String label, String[] args) {
-		if(cmd.equals("hi")){
+		if(cmd.getName().equalsIgnoreCase("hi")){
 			Player player = (Player) sender;
 			player.sendMessage(ChatColor.GOLD + "Hi");
-			
-			
+			return true;
 		}
 		
 		return false;
+	}
+	
+	@EventHandler
+	public void playerMove(PlayerMoveEvent event){
+	Player p = event.getPlayer();
+	Block block = event.getPlayer().getLocation().getBlock().getRelative(BlockFace.DOWN);
+	Material m = block.getType();
+	if(!(m == Material.GRASS)){
+		if(!(p.isFlying() == true)){
+		block.setType(Material.STONE);	
+	}
+	
+	}
+	
 	}
 
 }
