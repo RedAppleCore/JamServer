cb69ab74ed97a686d675ee09128341acb1993830
JamDev <jamesc2400@gmail.com>
2014-11-09 16:42:41 +0000

    Added title to 'server menu'

--- JamCadeMC/src/com/jam2400/jamcade/menu/ServerMenu.java
+++ JamCadeMC/src/com/jam2400/jamcade/menu/ServerMenu.java
@@ -1,6 +1,7 @@
 package com.jam2400.jamcade.menu;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -9,7 +10,7 @@ import com.jam2400.jamcade.items.ServerItem;
 
 public class ServerMenu {
 	public static Inventory getMenu(){
-		Inventory menu = Bukkit.createInventory(null, 9);
+		Inventory menu = Bukkit.createInventory(null, 9, ChatColor.GOLD + "Server Options:");
 		for(ServerItem i : ServerItem.values()){
 			int slot = i.getSlot();
 			ItemStack item = i.getItem();
